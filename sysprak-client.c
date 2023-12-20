1c1be95b79ea5d9f8b3598d7da20f7f849f1116f
ra35beq <liyan.qu@campus.lmu.de>
2023-12-20 20:12:31 +0100

    executive program

--- sysprak-client.c
+++ sysprak-client.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <unistd.h>
+#include <getopt.h>
 
 
+int main(int argc, char* argv[]){
 
-int main(){
+    int game_id =0;
+    int spielnummer = 0;
+
+    int ret;
+
+    while((ret = getopt(argc,argv,"g:p:")) != -1){
+
+        switch(ret){
+
+            case 'g':
+                game_id = atoi(optarg);
+                printf("optarg是%d\n",atoi(optarg));
+                break;
+            case 'p':
+                spielnummer = atoi(optarg);
+                printf("optarg是%d\n",atoi(optarg));
+                break;
+
+        }
+    }
+
+    printf("Game Id is :%d\n",game_id);
+    printf("Spielnummer is:%d\n",spielnummer);
+
 
 
 
