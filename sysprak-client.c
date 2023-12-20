606a956d0b17616af3dc131113e9d49e8394b249
ra35beq <liyan.qu@campus.lmu.de>
2023-12-20 21:12:25 +0100

    #define

--- sysprak-client.c
+++ sysprak-client.c
@@ -3,32 +3,47 @@
 #include <float.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <string.h>
 
+#define GAMEKINDNAME Checkers
+#define PORTNUMBER 1357
+#define HOSTNAME sysprak.priv.lab.nm.ifi.lmu.de
+
+
+void printHilfe(){
+
+    printf("Please enter your 13-digit Game ID and Spielernummer.");
+
+}
 
 int main(int argc, char* argv[]){
 
-    int game_id =0;
+    // Command Parameters as Game Id and Spielnummer
+    int game_id[13] = {0};
     int spielnummer = 0;
 
     int ret;
-
     while((ret = getopt(argc,argv,"g:p:")) != -1){
 
         switch(ret){
 
             case 'g':
-                game_id = atoi(optarg);
-                printf("optarg是%d\n",atoi(optarg));
+                printf("optarg is：%s\n",optarg);
+                printf("*optarg is：%c\n",*optarg);
+
+                
                 break;
             case 'p':
                 spielnummer = atoi(optarg);
-                printf("optarg是%d\n",atoi(optarg));
+                break;
+            default:
+                printHilfe();
                 break;
 
         }
     }
 
-    printf("Game Id is :%d\n",game_id);
+    printf("Game Id is :%d\n",atoi(game_id));
     printf("Spielnummer is:%d\n",spielnummer);
 
 
