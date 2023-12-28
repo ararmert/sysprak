1dc1b25dfdc09e667455a0a5c6c66ebc3e99a016
ra35beq <liyan.qu@campus.lmu.de>
2023-12-28 13:11:18 +0100

    28.12

--- sysprak-client.c
+++ sysprak-client.c
@@ -1,12 +1,12 @@
-#include <stdio.h>      // for printf()
+#include <stdio.h>      
 #include <stdlib.h>
 #include <float.h>
 #include <unistd.h>
-#include <getopt.h>     // for getopt()
+#include <getopt.h>     
 #include <string.h>
-#include <sys/types.h>  // for socket()
-#include <sys/socket.h> // for sockaddr
-#include <netinet/in.h> // for sockaddr_in
+#include <sys/types.h>  
+#include <sys/socket.h> 
+#include <netinet/in.h> 
 #include <arpa/inet.h> 
 #include <unistd.h>
 #include <stdlib.h>
@@ -23,16 +23,16 @@
 
 int main(int argc, char* argv[]){
 
-    // Command Parameters as Game Id and Spielnummer
+    // init Game-id and Spielernummer
     char game_id[13]={};
     int spielernummer = 0;
 
+    //analyse Command Parameters as Game-Id and Spielnummer
     int parameters;
     while((parameters = getopt(argc,argv,"g:p:")) != -1){
         int i = 0;
         int count = 0;
         switch(parameters){
-
             case 'g':
                 //Determines whether game id is 13 digits long.
                 while(*(optarg+i) != '\0'){
@@ -49,7 +49,6 @@ int main(int argc, char* argv[]){
                     }  
                 }                        
                 break;
-
             case 'p':
                 //Determines whether spielernummer is 1 or 2.
                 while(*(optarg+i) != '\0'){
@@ -68,6 +67,7 @@ int main(int argc, char* argv[]){
                 break;           
         }
     }
+
     //check game ID and Spielernummer
     printf("game id is:");
     for (int i = 0; i < 13; i++){
@@ -79,36 +79,33 @@ int main(int argc, char* argv[]){
 
     printf("<< Dame Client start! >>\n\n");
 
-    
+    // connect with Gameserver
+
     //get server ip address_server
     struct addrinfo hints;          
     struct addrinfo *result;        
-    int retern;                        
+    int return_server_ip;                        
     memset(&hints, 0, sizeof(struct addrinfo)); 
         hints.ai_flags = AI_PASSIVE;            
         hints.ai_family = AF_INET;              
         hints.ai_socktype = SOCK_STREAM;         
         hints.ai_protocol = 0;                  
-    
-    retern = getaddrinfo(HOSTNAME, NULL,&hints,&result); 
-    
-    if (retern < 0) {
-        fprintf(stderr, "%s\n", gai_strerror(retern));
+    return_server_ip = getaddrinfo(HOSTNAME, NULL,&hints,&result); 
+    if (return_server_ip < 0) {
+        fprintf(stderr, "%s\n", gai_strerror(return_server_ip));
         exit(1);
     }   
     struct sockaddr_in *addr;       
     addr = (struct sockaddr_in*)result->ai_addr;
     printf("server ip is : %s\n",inet_ntoa(addr->sin_addr));
     
-    
-
     //prepare socket
     int socket_client = socket(PF_INET,SOCK_STREAM,0);   
     int socket_server;                             
 
     struct sockaddr_in address_server;                 
     address_server.sin_family = AF_INET;
-    address_server.sin_port = htons(1357);
+    address_server.sin_port = htons(PORTNUMBER);
 
     // connect with server：
     address_server.sin_addr = addr->sin_addr;   
@@ -117,7 +114,7 @@ int main(int argc, char* argv[]){
     }else{
         printf("connection failed.\n");
     }
-
     freeaddrinfo(result);
+
     return 0;
 }
