29d02960e7cb28e5f560f0653539efd91ba882cf
Arar, Mert <arar.mert@campus.lmu.de>
2024-01-25 05:06:53 +0100

    Added line-by-line parsing for the server response to the player command to transform it into readable output and store it in shared memory.
    Created a new header file shared_data.h to store the struct Player, to be used in both performConnection.c and sysprak-client.c and utilized it.
    
    Enhanced error handling.
    
    Lost some sleep in the process :)

--- performConnection.c
+++ performConnection.c
@@ -1,119 +1,96 @@
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<sys/socket.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<stdbool.h>
-#include<errno.h>
-#include<signal.h>
-#include<netdb.h>
-#include<fcntl.h>
-#include "shared.h" 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <signal.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "shared_data.h" 
 
-#define GAMEKINDNAME "Checkers"
-#define PORTNUMBER "1357"
-#define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"                   
 #define BUFFER 256
 
+void performConnection(int socket_fd, char gameID[13], char playersend[9])
+{
 
-struct SharedData {
-     struct Player player;
-    char gameName[256];
-    
-     int totalPlayers;
-    pid_t thinkerPID;
-    pid_t connectorPID;
- };
-
-
-
-void handler(int s) {
-     printf("Caught SIGPIPE\n");
-}
-
-void thinker() {
-    printf("Thinker process has started.\n");
-
-
-    int status;
-    waitpid(-1, &status, 0);
-}
-
-// Prologphase der Kommunikation
-void performConnection(int socket_fd) {
-
-    // Receive the first server message
-    char* charbuffer = (char*)malloc(BUFFER * sizeof(char));
+    // Receive the first server message '+MNM'
+    char *charbuffer = (char *)malloc(BUFFER * sizeof(char));
     ssize_t size_received = recv(socket_fd, charbuffer, BUFFER - 1, 0);
-    if (size_received > 0) { 
+    if (size_received > 0)
         charbuffer[size_received] = '\0';
-        printf("%s\n", charbuffer);
-    } else {
-        perror("Error receiving the first server message.");
-        close(socket_fd);
-        exit(EXIT_FAILURE);
-    }
+    printf("%s\n", charbuffer);
 
-
-     // Set the socket to non-blocking mode
+    // Set the socket to non-blocking mode
     int flags = fcntl(socket_fd, F_GETFL, 0);
-    if (flags == -1) {
+    if (flags == -1)
+    {
         perror("Error getting socket flags");
         close(socket_fd);
         exit(EXIT_FAILURE);
     }
-
-    if (fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+    if (fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1)
+    {
         perror("Error setting socket to non-blocking mode");
         close(socket_fd);
         exit(EXIT_FAILURE);
     }
 
     // Send the client version to the server
-    char clientVersion[] = "VERSION 3.1\n"; 
+    char clientVersion[] = "VERSION 3.3\n";
     ssize_t sent_byte = send(socket_fd, clientVersion, strlen(clientVersion), 0);
 
-    if(sent_byte == -1) {
+    if (sent_byte == -1)
+    {
         perror("Error sending client version.\n");
         close(socket_fd);
         exit(EXIT_FAILURE);
-    
-    } else { 
+    }
+    else
+    {
         printf("%zd bytes sent(Client Version).\n", sent_byte);
-        
+    }
 
-    }  
-
-    // Add a delay
     usleep(500000);
-    
 
+    // Set a timeout for receiving server responses (work in progress)
+    // struct timeval timeout;
+    // timeout.tv_sec = 5;
+    // timeout.tv_usec = 0;
 
-    
+    // fd_set readfds;
+    // FD_ZERO(&readfds);
+    // FD_SET(socket_fd, &readfds);
 
-     // Wait for the socket to be ready for reading or until timeout
-    // if (waitForSocket(socket_fd, 5) == 0) {
+    // // Wait for the socket to be ready for reading or until timeout
+    // int ready = select(socket_fd + 1, &readfds, NULL, NULL, &timeout);
+
+    // if (ready == -1) {
+    //     perror("Error in select");
+    //     close(socket_fd);
+    //     exit(EXIT_FAILURE);
+    // } else if (ready == 0) {
+    //     // Timeout occurred
     //     perror("Timeout waiting for server response");
     //     close(socket_fd);
     //     exit(EXIT_FAILURE);
     // }
 
-
-
-    // Receive server response
-    char* charbufferr = (char*)malloc(BUFFER * sizeof(char));
+    // Receive server response about Client Version
+    char *charbufferr = (char *)malloc(BUFFER * sizeof(char));
     ssize_t size_receivedd = recv(socket_fd, charbufferr, BUFFER - 1, 0);
     if (size_receivedd > 0) {
         charbufferr[size_receivedd] = '\0';
-        printf("%s\n", charbufferr);
-    
+    printf("%s\n", charbufferr);
+
     } else {
         perror("Error receiving server response after sending client version.");
         close(socket_fd);
@@ -121,83 +98,82 @@ void performConnection(int socket_fd) {
     }
 
 
+
     // Send the Game-ID to the server
-    char gameID[] = "ID 0sk8wc9exo1g1\n";
-    ssize_t sent_gameid = send(socket_fd, gameID, strlen(gameID), 0);
-
-
-    if(sent_gameid == -1) {
+    char gameIDs[18] = "ID ";
+    strcat(gameIDs, gameID);
+    strcat(gameIDs, "\n");
+    printf("game id is %s\n", gameIDs);
+    ssize_t sent_gameid = send(socket_fd, gameIDs, strlen(gameIDs), 0);
+    if (sent_gameid == -1)
+    {
         perror("Error sending Game-ID.\n");
         close(socket_fd);
         exit(EXIT_FAILURE);
-    } else { 
+    }
+    else
+    {
         printf("%zd bytes sent(Game-ID).\n", sent_byte);
     }
-
-    // Add a delay (bunu perror icinde yapmaya calis)
+    // Add a delay
     usleep(500000);
 
-   
-
-    // Receive server response
-    char* charbufferrr = (char*)malloc(BUFFER * sizeof(char));
+    // Receive server response about Gamekind-Name
+    char *charbufferrr = (char *)malloc(BUFFER * sizeof(char));
     ssize_t size_receiveddd = recv(socket_fd, charbufferrr, BUFFER - 1, 0);
     if (size_receiveddd > 0) {
-    charbufferrr[size_receiveddd] = '\0';
+        charbufferrr[size_receiveddd] = '\0';
     printf("%s\n", charbufferrr);
-    
-    char gName[20];
-    strncpy(gName, &charbufferrr[10], 8);
-    gName[8] = '\0';
-    
+    } else {
+        perror("Error receiving server response about the game kind.");
+        close(socket_fd);
+        exit(EXIT_FAILURE);
 
-    // Exit if game kind is not Checkers
+    }
+
+    // Quit if Gamekind is not 'Checkers'
+    char gkindName[20];
+    strncpy(gkindName, &charbufferrr[10], 8);
+    gkindName[8] = '\0';
+
     char strcheck[] = "Checkers";
-    
-    int compare = strcmp(gName, strcheck);
-    if (compare != 0) {
+
+    int compare = strcmp(gkindName, strcheck);
+    if (compare != 0)
+    {
         perror("Wrong GameKindName received from the server.\n");
         close(socket_fd);
         exit(EXIT_FAILURE);
     }
-    
 
+    // save Gamename
+    char gName[102];
+    strncpy(gName, &charbufferrr[21], 100);
+    gName[101] = '\0';
 
-    } else {
-        perror("Error receiving server response after sending Game-ID");
-        close(socket_fd);
-        exit(EXIT_FAILURE);
-    }
-
-   
-    
-
-    
-
+    printf("game Name from Webinterface is %s\n",gName);
     
     // Send the PLAYER command to the server
-    char playerKommando[] = "PLAYER \n";
-    ssize_t sent_bytes = send(socket_fd, playerKommando, strlen(playerKommando), 0);
+    ssize_t sent_bytes = send(socket_fd, playersend, strlen(playersend), 0);
 
-    if (sent_bytes == -1) {
+    if (sent_bytes == -1)
+    {
         perror("Playerkommando konnte nicht gesendet werden.");
         fprintf(stderr, "Error code: %d\n", errno);
         close(socket_fd);
         exit(EXIT_FAILURE);
-    } else printf("%zd bytes sent(Player).\n", sent_bytes);
-
+    }
+    else
+        printf("%zd bytes sent(Player).\n", sent_bytes);
 
     // Add a delay
     usleep(500000);
 
-
-
     struct Player player = {.totalPlayerNum = 2};
-    
-
 
     // Receive server response after sending PLAYER command.
-    char quickbuffer[50];
+    
+      char quickbuffer[50];
     char* playerbuffer = (char*)malloc(BUFFER * sizeof(char));
     ssize_t player_received = recv(socket_fd, playerbuffer, BUFFER - 1, 0);
 
@@ -206,6 +182,8 @@ void performConnection(int socket_fd) {
         snprintf(quickbuffer, sizeof(quickbuffer), "%.42s", playerbuffer); // store the needed information from the server response in quickbuffer
 
         char* line = strtok(quickbuffer, "\n");
+
+        // Line-by-line parsing for formatting default server responses into readable language.
 
     while (line != NULL) {
         if (sscanf(line, "+ YOU %d Player %d", &player.playerNum, &player.playerName) == 2) {
@@ -238,173 +216,19 @@ void performConnection(int socket_fd) {
         // printf("%d\n", player_received); to print the size of buffer being used
     
 
-    free(playerbuffer);
-    
-
-
-
-
-
-
-
-       
- 
-
-    // Close the socket when done
-    close(socket_fd);
-
-}
-
-void connector(int socket_fd) {
-    printf("Connector process has started.\n");
-
-    performConnection(socket_fd);
-
-    exit(EXIT_SUCCESS);
-}
-
-
-
-// Main function
-int main(int argc, char** argv) {
-    signal(SIGPIPE, handler);
-
-
-    int socket_fd;
-    char* charbuffer = (char*)malloc(BUFFER * sizeof(char));
-    ssize_t size;
-
-    // Create a TCP socket
-    //int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    //if (socket_fd == -1) {
-      //  perror("Failed to create socket");
-       // exit(EXIT_FAILURE);
-    //} else if (socket_fd == 3) {
-
-      //  printf("Socket created successfully.\n");
-    //}
-
-
-
-
-    // Placeholder code up until the creation of the Elternprozess
-    // TODO Replace with own implementation
-    
-
-
-
-
-
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(struct addrinfo));
-    struct addrinfo* results = NULL;
-    struct addrinfo* record = NULL;
-
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if (getaddrinfo(HOSTNAME, PORTNUMBER, &hints, &results) != 0) {
-        perror("Failed to translate client socket.");
-        return -1;
-    }
-
-    for (record = results; record != NULL; record = record->ai_next) {
-        socket_fd = socket(record->ai_family, record->ai_socktype, record->ai_protocol);
-         if (socket_fd == -1) {
-        perror("Error creating socket");
-        continue;  
-    }
-
-        if (connect(socket_fd, record->ai_addr, record->ai_addrlen) != -1) {
-
-            printf("Connected to the server: \n");
-            break;
-
-        } else {
-            perror("Connection failed. \n");
-            close(socket_fd);
-            exit(EXIT_FAILURE);
-        }
-}
-
-   freeaddrinfo(results);
-
-    // Elternprozess
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        perror("Error forking process.");
-        exit(EXIT_FAILURE);
-
-    } else if (pid == 0) {
-        // Kindprozess (Connector)
-
-        // Shared Memory Bereich
-        int shm_id = shmget(IPC_PRIVATE, sizeof(struct SharedData), IPC_CREAT | 0666);
-        if (shm_id < 0) {
-            perror("Error creating shared memory segment.");
-            exit(EXIT_FAILURE);
-        } else if (shm_id > 0) {
-        // Shared Memory Attach
-
-        struct SharedData* sharedData = (struct SharedData*)shmat(shm_id, NULL, 0);
-
-        
-        if ((intptr_t)sharedData == -1) {
-            perror("Error attaching shared memory");
-            exit(EXIT_FAILURE);
-        }
-
-        //Initialize shared data
-        strcpy(sharedData->gameName, GAMEKINDNAME);
-        sharedData->player.playerNum = 1;
-        sharedData->totalPlayers = 2;
-        sharedData->thinkerPID = getpid();
-        sharedData->connectorPID = 0;
-
-        if(shmdt(sharedData) == -1) {
-            perror("Error detaching shared memory");
-            exit(EXIT_FAILURE);
-        } exit(EXIT_SUCCESS);
-
-        if(shmctl(shm_id, IPC_RMID, NULL) == -1) {
-            perror("Error removing shared memory");
-            exit(EXIT_FAILURE);
-        } exit(EXIT_SUCCESS);
-
-        
-
-
-
-    }
-
-}
-        connector(socket_fd);
-        exit(EXIT_SUCCESS);
-    
-        // Elternprozess
-        thinker();
-        // Warte auf die Terminierung des Kindprozesses
-        int status;
-        waitpid(pid, &status, 0);
-    
-    
-    
-    size = recv(socket_fd, charbuffer, BUFFER - 1, 0);
-    if (size > 0) charbuffer[size] = '\0';
-    printf("Server message: %s", charbuffer);
-
-    
-
-    close(socket_fd);
     free(charbuffer);
-    
+    free(charbufferr);
+    free(charbufferrr);
+    free(playerbuffer);
+    // free(quickbuffer) if it's not used in the main function later on
 
-    return 0;
 
 
-}
+}  
+
+
+
+
 
 
 
