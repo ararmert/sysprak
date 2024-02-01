bb185b279aefd4dc608b7db8e08200cfa6f8b20b
Arar, Mert <arar.mert@campus.lmu.de>
2024-02-01 19:19:26 +0100

    Replace sysprak-client.c

--- sysprak-client.c
+++ sysprak-client.c
@@ -1,6 +1,7 @@
 #include <stdio.h>      
 #include <stdlib.h>
 #include <float.h>
+#include <string.h> 
 #include <unistd.h>
 #include <getopt.h>     
 #include <string.h>
@@ -20,6 +21,7 @@
 #include "handler.h"
 #include "performConnection.h"
 #include "config.h"
+#include "shared_data.h"
 
 #define BUFFER 256
 
@@ -30,28 +32,16 @@ struct config{
     char GameKindName[256];
 };
 
-struct Player{
-    int playerNum;
-    int playerName;
-    bool isReady;
-};
-
-struct SharedData{
-    struct Player player;
-    char gameName[32];
-    int totalPlayers;
-    pid_t thinkerPID;
-    pid_t connectorPID;
-};
-
 
 int main(int argc, char* argv[]){
 
     signal(SIGPIPE, handler);
     
     // init Game-id and Spielernummer
-    char gameID[13]="";
-    int spielernummer;
+    char *gameID = (char*) malloc (sizeof(char)* 13); //to avoid avoid stack confusion
+    
+    // char gameID[13]="";
+    int spielernummer = -1;
    // char parameterName[256];
     struct config config_server = {"un know",0,"un know"};
     char fileName[256]="client.conf\0";
@@ -63,21 +53,32 @@ int main(int argc, char* argv[]){
         int count = 0;
         switch(parameters){
             case 'g':
-                //Determines whether game id is 13 digits long.
-                while(*(optarg+i) != '\0'){
-                    i++;
-                    count++;
-                }                 
-                if(count != 13){
+                if(strlen(optarg)==13){
+                    printf("%s\n optarg: ",optarg);
+                    strcpy(gameID, optarg);
+                } else {   
                     printf("Game ID should be 13 digits lang.\n");
-                    return -1;
-                }else{
-                    i = 0;
-                    while(*(optarg+i) != '\0'){
-                        gameID[i] = *(optarg+i);
-                        i++;
-                    }  
-                }                        
+                    return -1; }
+
+                   
+                
+
+                //Determines whether game id is 13 digits long.
+                // while(*(optarg+i) != '\0'){
+                //     i++;
+                //     count++;
+                // }                 
+                // if(count != 13){
+                //     printf("Game ID should be 13 digits lang.\n");
+                //     return -1;
+                // }else{
+                //     i = 0;
+                //     while(*(optarg+i) != '\0'){
+                //         gameID[i] = *(optarg+i);
+                //         i++;
+                //         printf("Game ID: %s\n", gameID);
+                //     }  
+                                        
                 break;
             case 'p':
                 //Determines whether spielernummer is 0 or 1.
@@ -107,25 +108,33 @@ int main(int argc, char* argv[]){
     printf("game id is:");
     for (int i = 0; i < 13; i++){
         printf("%c",gameID[i]);
+    
     }
+    
+  
     puts("");   
     printf("Spielernummer is:%d\n", spielernummer);
     printf("before palyer send");
 
+    printf("GAMEID TEST BEFORE: %s\n", gameID);
+
     char playersend[15] = "PLAYER ";
-    if(spielernummer == 0 ){
-        strcat(playersend, "0\n");
-    }else if(spielernummer == 1){
-        strcat(playersend, "1\n");
-    }else{
-        strcat(playersend, " \n");
-    }
-    printf("playersend is %s\n",playersend);
+     if(spielernummer == 0 ){
+         strcat(playersend, "0\n");
+     }else if(spielernummer == 1){
+         strcat(playersend, "1\n");
+     }else{
+         strcat(playersend, " \n");
+     }
+     printf("playersend is %s\n",playersend);
+     printf("GAMEID TEST AFTER: %s\n", gameID);
+
+     
     
     //check conf file setting
-    printf("before, Hostname is: %s\n",config_server.Hostname);
-    printf("before, Posrtnumber is: %d\n",config_server.PortNumer); 
-    printf("before, GameKindName is: %s\n\n",config_server.GameKindName);
+    // printf("before, Hostname is: %s\n",config_server.Hostname);
+    // printf("before, Posrtnumber is: %d\n",config_server.PortNumer); 
+    // printf("before, GameKindName is: %s\n\n",config_server.GameKindName);
 
     config(fileName,&config_server);  
 
@@ -140,7 +149,10 @@ int main(int argc, char* argv[]){
     printf("after, GameKindNameis :%s\n",config_server.GameKindName); 
     
     
+
     printf("<< Dame Client start! >>\n\n");
+
+    struct SharedData *sharedData;
 
     // Shared Memory Bereich
     int shm_id = shmget(IPC_PRIVATE, sizeof(struct SharedData), IPC_CREAT | 0666); 
@@ -149,7 +161,7 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }else {
         // Shared Memory Attach
-        struct SharedData *sharedData = (struct SharedData *)shmat(shm_id, NULL, 0);
+        sharedData = (struct SharedData *)shmat(shm_id, NULL, 0);
         if ((intptr_t)sharedData == -1){
             perror("Error attaching shared memory");
             exit(EXIT_FAILURE);
@@ -161,8 +173,23 @@ int main(int argc, char* argv[]){
         sharedData->totalPlayers = 2;
         sharedData->thinkerPID = getpid();
         sharedData->connectorPID = 0;
+        
+
+        
+
+        
     }
+
     
+    
+    int pipe_fds[2];
+
+    // pipe-Erstellung
+    if(pipe(pipe_fds) == -1) {
+        perror("Pipe konnte nicht erstellt werden.");
+        exit(EXIT_FAILURE);
+    }
+
     pid_t pid  = fork();
 
 
@@ -172,8 +199,10 @@ int main(int argc, char* argv[]){
     } 
     // parentprocess
     if (pid > 0) {
+        
         printf("Parent process beginn.\n");
         //wait for childprocess
+        close(pipe_fds[0]);
         pid = waitpid(pid, NULL, 0);
         printf("parent process end.\n");
         if (pid < 0) {
@@ -181,10 +210,10 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
         }
 
-        /* if (shmdt(sharedData) == -1){
-            perror("Error detaching shared memory");
-            exit(EXIT_FAILURE);
-    } */
+        if (shmdt(sharedData) == -1){
+             perror("Error detaching shared memory");
+             exit(EXIT_FAILURE);
+    } 
         
     if (shmctl(shm_id, IPC_RMID, NULL) == -1){
         perror("Error removing shared memory");
@@ -199,6 +228,9 @@ int main(int argc, char* argv[]){
         printf("Child process beginn.\n"); 
         // connect with Gameserver
         //get server ip address_server
+        close(pipe_fds[1]);
+        struct SharedData *sharedData = (struct SharedData *)shmat(shm_id, NULL, 0);
+        sharedData->connectorPID = getpid();
 
         struct addrinfo hints;          
         struct addrinfo* results;        
@@ -241,7 +273,15 @@ int main(int argc, char* argv[]){
 
         freeaddrinfo(results);
 
-        performConnection(socket_fd,gameID,playersend);
+        printf("%s\n", gameID);
+        printf("%s\n", playersend);
+
+        FILE* readFile = fdopen(socket_fd, "r");
+        
+
+        
+
+        performConnection(socket_fd,gameID,playersend,shm_id,readFile);
 
         /* char* charbuffer = (char*)malloc(BUFFER * sizeof(char));
         ssize_t size;
@@ -252,7 +292,7 @@ int main(int argc, char* argv[]){
         close(socket_fd);
         //free(charbuffer);
 
-        /* if (shmdt(sharedData) == -1){
+         if (shmdt(sharedData) == -1){
             perror("Error detaching shared memory");
             exit(EXIT_FAILURE);
     } 
@@ -261,8 +301,8 @@ int main(int argc, char* argv[]){
         perror("Error removing shared memory");
         exit(EXIT_FAILURE);
     }else{
-        printf("SHM delete \n");
-    } */
+        printf("SHM deleted. \n");
+    } 
         
     printf("Child process end.\n");
     }
