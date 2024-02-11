2b12840e2586348356a49fb4287baacf4101f8dc
Arar, Mert <arar.mert@campus.lmu.de>
2024-02-11 15:56:42 +0100

    Replace signalHandler.c

--- signalHandler.c
+++ signalHandler.c
@@ -5,66 +5,69 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <signal.h>
-#include <shm.h> 
+//#include <shm.h> 
 #include <shared_data.h>
-#include <board.h>
+#include "board.h"
 
-static struct SharedData sharedData = NULL;
-static struct YililiaSHMspielstand = NULL;
+static struct SharedData *sharedData = NULL;
+static struct Piece *pieces = NULL;
 
 void signalHandler(){
     if(sharedData->shouldThink){
         int i = 0;
         int blackCount = 0;
         int whiteCount = 0;
-        while i < (sizeof(spielstand)/sizeof(struct Piece)){
-            if(spielstand[i].colour == black){
+        int numberofOwnStones;
+        int numberOfOpponentStones;
+        while (i < (sizeof(pieces)/sizeof(struct Piece))){
+            if(pieces[i].color == black){
                 blackCount++;
             }
-            else if(spielstand[i].colour == white){
+            else if(pieces[i].color == white){
                 whiteCount++;
             }
             i++;
         }
 
-        if(Player->playerNum == 0){
+        if(sharedData->player.playerNum == 0){
             int numberofOwnStones = whiteCount;
             int numberOfOpponentStones = blackCount;
         }
         else{
             int numberofOwnStones = blackCount;
-            int numberOfOpponentStones = whiteCount
+            int numberOfOpponentStones = whiteCount;
         }
 
-        struct Spielsteine own = (struct Spielsteine) malloc(sizeof(struct Piece)* numberofOwnStones);
-        struct Spielsteine opponent = (struct Spielsteine) malloc(sizeof(struct Piece)* numberOfOpponentStones);
+        struct Piece *own =  malloc(sizeof(struct Piece) * numberofOwnStones);
+        struct Piece *opponent = malloc(sizeof(struct Piece) * numberOfOpponentStones);
 
-        int i = 0
-        int self = 0
-        int opp = 0
-        while i < (sizeof(spielstand)/sizeof(struct Piece)){
-            if(sharedData->spielernummer == 0 && spielstand[i].colour == white){
-                own[self]= spielstand[i];
+        int i = 0;
+        int self = 0;
+        int opp = 0;
+        while (i < (sizeof(pieces)/sizeof(struct Piece))){
+            if(sharedData->player.playerNum == 0 && pieces[i].color == white){
+                own[self] = pieces[i];
                 self++;
             }
         }
-        while i < (sizeof(spielstand)/sizeof(struct Piece)){
-            if(sharedData->spielernummer == 0 && spielstand[i].colour == black){
-                opponent[opponent]= spielstand[i];
+        while (i < (sizeof(pieces)/sizeof(struct Piece))){
+            if(sharedData->player.playerNum == 0 && pieces[i].color == black){
+                opponent[opp] = pieces[i];
                 opp++;
             }
         }
-        while i < (sizeof(spielstand)/sizeof(struct Piece)){
-            if(sharedData->spielernummer == 1 && spielstand[i].colour == black){
-                own[self]= spielstand[i];
+        while (i < (sizeof(pieces)/sizeof(struct Piece))){
+            if(sharedData->player.playerNum == 1 && pieces[i].color == black){
+                own[self] = pieces[i];
                 self++;
             }
         }
-        while i < (sizeof(spielstand)/sizeof(struct Piece)){
-            if(sharedData->spielernummer == 1 && spielstand[i].colour == white){
-                opponent[opp]= spielstand[i];
+        while (i < (sizeof(pieces)/sizeof(struct Piece))){
+            if(sharedData->player.playerNum == 1 && pieces[i].color == white){
+                opponent[opp] = pieces[i];
                 opp++;
             }
+            
         }
         
             
